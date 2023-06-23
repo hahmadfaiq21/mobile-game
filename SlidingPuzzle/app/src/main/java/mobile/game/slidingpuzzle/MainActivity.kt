d2b3ee4a58cd9a6d382e75512f2e513bc810c120
Husain Ahmad Faiq <74751720+hahmadfaiq21@users.noreply.github.com>
2023-06-23 20:25:46 +0700

    Add files via upload

---
+++ SlidingPuzzle/app/src/main/java/mobile/game/slidingpuzzle/MainActivity.kt
@@ -0,0 +1,96 @@
+package mobile.game.slidingpuzzle
+
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var requestCode = 0
+    private lateinit var preferences: Preferences
+
+    private val startGameLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // Handle the result here
+            loadData()
+            loadMode()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
+
+        preferences = Preferences(this)
+
+        val buttonStartGame = findViewById<Button>(R.id.button_start_game)
+        buttonStartGame.setOnClickListener {
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startGameLauncher.launch(intent)
+            finish()
+        }
+
+        val switchDarkMode = findViewById<SwitchCompat>(R.id.switch_dark_mode)
+        switchDarkMode.setOnCheckedChangeListener { _, b ->
+            switchDarkMode.isChecked = b
+            preferences.saveMode(b)
+            if (preferences.getMode()) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            setResult(RESULT_OK)
+        }
+
+        switchDarkMode.isChecked = preferences.getMode()
+        loadMode()
+        loadData()
+    }
+
+    private fun loadMode() {
+        if (preferences.getMode()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
+    private fun loadData() {
+        val textLastStep = findViewById<TextView>(R.id.text_last_step)
+        textLastStep.text = preferences.getLastStep().toString()
+
+        val textLastTime = findViewById<TextView>(R.id.text_last_time)
+        val lastTimePref = preferences.getLastTime()
+        val lastSecond = lastTimePref % 60
+        val lastHour = lastTimePref / 3600
+        val lastMinute = (lastTimePref - lastHour * 3600) / 60
+        textLastTime.text = String.format("%02d:%02d:%02d", lastHour, lastMinute, lastSecond)
+
+        val textBestStep = findViewById<TextView>(R.id.text_best_step)
+        textBestStep.text = preferences.getBestStep().toString()
+
+        val textBestTime = findViewById<TextView>(R.id.text_best_time)
+        val bestTimePref = preferences.getBestTime()
+        val bestSecond = bestTimePref % 60
+        val bestHour = bestTimePref / 3600
+        val bestMinute = (bestTimePref - bestHour * 3600) / 60
+        textBestTime.text = String.format("%02d:%02d:%02d", bestHour, bestMinute, bestSecond)
+    }
+    
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == this.requestCode) {
+            loadData()
+            loadMode()
+        }
+    }
+}
